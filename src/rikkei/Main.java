172ed20baed8db244340bd2e311fa69700e2eec5
minh22091996 <tranbinhminh22091996@gmail.com>
2023-03-08 00:15:09 +0700

    Initial commit

---
+++ src/rikkei/Main.java
@@ -0,0 +1,16 @@
+package rikkei;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+
+
+    }
+
+}
+
